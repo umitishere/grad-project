d10b7cdcc53b907baaf667fae64786e4f2d633d8
umitishere <umitsancakli98@gmail.com>
2022-03-22 21:19:58 +0300

    Update db credentials

--- includes/config.php
+++ includes/config.php
@@ -8,12 +8,19 @@ if ($_SERVER["SERVER_NAME"] == "localhost") {
     $rootPath = "/";
 }
 
-require_once("DB_CREDENTIALS.php");
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', $DUMMY_USERNAME);
-define('DB_PASSWORD', $DUMMY_PASSWORD);
-define('DB_NAME', $DUMMY_DB);
+define('DB_SERVER', $cleardb_server);
+define('DB_USERNAME', $cleardb_username);
+define('DB_PASSWORD', $cleardb_password);
+define('DB_NAME', $cleardb_db);
 
 /* Attempt to connect to MySQL database */
 try{
