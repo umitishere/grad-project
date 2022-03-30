71b7ae4f2458342ef149f2bd5d3bfb7ab6b4be46
umitishere <umitsancakli98@gmail.com>
2022-03-30 18:13:54 +0300

    Fix login redirect bug

--- includes/header.php
+++ includes/header.php
@@ -52,56 +52,6 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
     <link href="<?php echo $rootPath; ?>/assets/css/images.css" rel="stylesheet" />
 
     <title><?php echo $pageTitle; ?></title>
-
-    <script>
-
-    $(document).ready(function() {
-
-        $("#searchUser").on('shown.bs.modal', function(){
-            $("#search").focus();
-        });
-
-
-        //On pressing a key on "Search box" in "search.php" file. This function will be called.
-        $("#search").keyup(function() {
-
-            //Assigning search box value to javascript variable named as "name".
-            var name = $('#search').val();
-
-            //Validating, if "name" is empty.
-            if (name == "") {
-                //Assigning empty value to "display" div in "search.php" file.
-                $("#display").html("");
-            } else {
-
-                //AJAX is called.
-                $.ajax({
-
-                    //AJAX type is "Post".
-                    type: "POST",
-
-                    //Data will be sent to "ajax.php".
-                    url: "ajax.php",
-
-                    //Data, that will be sent to "ajax.php".
-                    data: {
-
-                        //Assigning value of "name" into "search" variable.
-                        search: name
-                    },
-
-                    // If result found, this funtion will be called.
-                    success: function(html) {
-
-                        //Assigning result to "display" div in "search.php" file.
-                        $("#display").html(html).show();
-                    }
-               });
-           }
-       });
-    });
-
-    </script>
 </head>
 <body>
 
